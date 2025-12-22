2d9e231a9244d437cd775e3666aae45e0c8661c9
baonoob1011 <trandinhbao222@gmail.com>
2025-12-22 20:08:46 +0700

    apply redis ceche exam and answer when submit delete key

---
+++ learningApp/src/main/java/com/example/learningApp/configuration/RedisConfig.java
@@ -0,0 +1,40 @@
+package com.example.learningApp.configuration;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        // Jackson serializer
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule()); // support LocalDateTime
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        serializer.setObjectMapper(mapper);
+
+        template.setValueSerializer(serializer);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
--- learningApp/src/main/java/com/example/learningApp/controller/exam/ExamController.java
+++ learningApp/src/main/java/com/example/learningApp/controller/exam/ExamController.java
@@ -94,7 +94,7 @@ public class ExamController {
         return ResponseEntity.ok(
                 ApiResponse.success(
                         "Exam submitted successfully",
-                        submitService.submitExam(request)
+                        submitService.submitExam(request.getParticipantId())
                 )
         );
     }
