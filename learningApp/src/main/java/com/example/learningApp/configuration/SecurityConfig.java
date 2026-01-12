c9a4269ad30fa9592ac6a0efd9ec42a2ce0ddf1a
Nguyen Hoai An <annguyen@gmail.com>
2026-01-12 14:27:32 +0700

    [Feat]: add feature for user

--- learningApp/src/main/java/com/example/learningApp/configuration/SecurityConfig.java
+++ learningApp/src/main/java/com/example/learningApp/configuration/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     CustomAccessDeniedHandler accessDeniedHandler;
     private final String[] PUBLIC_ENDPOINTS = {
        "/api/v1/auth/**",
-       "/api/v1/users/register",
+       "/api/v1/users/register", "/api/v1/users/forgot-password", "/api/v1/users/confirm-forgot-password",
     };
 
 
