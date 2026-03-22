22a5d2a333f4b7f39c5f8ddd5daa94c0f0e0b29e
baonoob1011 <trandinhbao222@gmail.com>
2026-03-22 23:36:01 +0700

    Update AwsConfig.java

--- learningApp/src/main/java/com/example/learningApp/configuration/AwsConfig.java
+++ learningApp/src/main/java/com/example/learningApp/configuration/AwsConfig.java
@@ -1,6 +1,5 @@
 package com.example.learningApp.configuration;
 
-
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
@@ -23,78 +22,75 @@ import software.amazon.awssdk.services.translate.TranslateClient;
 @Configuration
 public class AwsConfig {
 
-    @Value("${aws.iam.access-key}")
-    private String accessKey;
+        @Value("${aws.iam.access-key}")
+        private String accessKey;
 
-    @Value("${aws.iam.secret-key}")
-    private String secretKey;
+        @Value("${aws.iam.secret-key}")
+        private String secretKey;
 
-    @Value("${aws.iam-nam.access-key}")
-    private String accessKeyNam;
+        @Value("${aws.iam-nam.access-key}")
+        private String accessKeyNam;
 
-    @Value("${aws.iam-nam.secret-key}")
-    private String secretKeyNam;
+        @Value("${aws.iam-nam.secret-key}")
+        private String secretKeyNam;
 
-    @Value("${aws.region}")
-    private String region;
+        @Value("${aws.region}")
+        private String region;
 
-    @Bean
-    public S3Client s3Client() {
-        System.out.println("AccessKeyNam: " + accessKeyNam);
-        return S3Client.builder()
-                .region(Region.US_EAST_1)
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKeyNam, secretKeyNam)
-                        )
-                )
-                .build();
-    }
+        @Bean
+        public S3Client s3Client() {
+                System.out.println("AccessKeyNam: " + accessKeyNam);
+                return S3Client.builder()
+                                .region(Region.US_EAST_1)
+                                .credentialsProvider(
+                                                StaticCredentialsProvider.create(
+                                                                AwsBasicCredentials.create(accessKeyNam, secretKeyNam)))
+                                .build();
+        }
 
-    @Bean
-    public PollyClient pollyClient() {
-        // Thay YOUR_ACCESS_KEY và YOUR_SECRET_KEY bằng key thực tế
-        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
-                accessKeyNam,
-                secretKeyNam
-        );
+        @Bean
+        public PollyClient pollyClient() {
+                // Thay YOUR_ACCESS_KEY và YOUR_SECRET_KEY bằng key thực tế
+                AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
+                                accessKeyNam,
+                                secretKeyNam);
 
-        return PollyClient.builder()
-                .region(Region.US_EAST_1) // chọn region phù hợp
-                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                .build();
-    }
-    @Bean
-    public AmazonS3 amazonS3() {
-        System.out.println("AccessKeyVy: " + accessKey);
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(Regions.fromName(region))   
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                .build();
-    }
-    @Bean
-    public TranscribeClient transcribeClient() {
-        return TranscribeClient.builder()
-                .region(Region.US_EAST_1)
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKeyNam, secretKeyNam)
-                        )
-                )
-                .build();
-    }
-    @Bean
-    public TranslateClient translateClient() {
-        return TranslateClient.builder()
-                .region(Region.US_EAST_1)
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKeyNam, secretKeyNam)
-                        )
-                )
-                .build();
-    }
+                return PollyClient.builder()
+                                .region(Region.US_EAST_1) // chọn region phù hợp
+                                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                                .build();
+        }
+
+        @Bean
+        public AmazonS3 amazonS3() {
+                System.out.println("AccessKeyVy: " + accessKey);
+                System.out.println("code mới nè vnpay: ");
+
+                BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+                return AmazonS3ClientBuilder.standard()
+                                .withRegion(Regions.fromName(region))
+                                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                                .build();
+        }
+
+        @Bean
+        public TranscribeClient transcribeClient() {
+                return TranscribeClient.builder()
+                                .region(Region.US_EAST_1)
+                                .credentialsProvider(
+                                                StaticCredentialsProvider.create(
+                                                                AwsBasicCredentials.create(accessKeyNam, secretKeyNam)))
+                                .build();
+        }
+
+        @Bean
+        public TranslateClient translateClient() {
+                return TranslateClient.builder()
+                                .region(Region.US_EAST_1)
+                                .credentialsProvider(
+                                                StaticCredentialsProvider.create(
+                                                                AwsBasicCredentials.create(accessKeyNam, secretKeyNam)))
+                                .build();
+        }
 
 }
-
