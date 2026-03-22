4cd5bfd4fff553017552095936376764cd03832f
baonoob1011 <trandinhbao222@gmail.com>
2026-03-22 11:33:55 +0700

    Update IncomingCallNotification.java

--- learningApp/src/main/java/com/example/learningApp/dto/IncomingCallNotification.java
+++ learningApp/src/main/java/com/example/learningApp/dto/IncomingCallNotification.java
@@ -5,10 +5,10 @@ import lombok.Data;
 @Data
 public class IncomingCallNotification {
     private String type = "incoming";
+    private String callType = "VIDEO"; // VIDEO or VOICE
     private String roomId;
     private String callerId;
     private String callerName;
     private String callerAvatar;
     private String receiverId;
 }
-
