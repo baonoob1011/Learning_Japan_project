22730ed1c53fcb9519eca6eceb4a92d14b8f7180
baonoob1011 <trandinhbao222@gmail.com>
2026-03-22 16:15:53 +0700

    Update ChatMessageResponse.java

--- learningApp/src/main/java/com/example/learningApp/dto/response/chat/ChatMessageResponse.java
+++ learningApp/src/main/java/com/example/learningApp/dto/response/chat/ChatMessageResponse.java
@@ -2,15 +2,16 @@ package com.example.learningApp.dto.response.chat;
 
 import lombok.Data;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
 @Data
 public class ChatMessageResponse {
     private String roomId;
-    private String senderId;   // ✅ thêm dòng này
+    private String senderId; // ✅ thêm dòng này
     private String senderName;
     private String content;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime sentAt;
 }
-
-
