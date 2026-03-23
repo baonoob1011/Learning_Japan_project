86528981419e769798b44e259ec9645f5d3c0e0d
baonoob1011 <trandinhbao222@gmail.com>
2026-03-23 19:24:56 +0700

    Update YoutubeVideoService.java

--- learningApp/src/main/java/com/example/learningApp/service/video/YoutubeVideoService.java
+++ learningApp/src/main/java/com/example/learningApp/service/video/YoutubeVideoService.java
@@ -257,11 +257,12 @@ public class YoutubeVideoService {
                 ytDlpPath,
                 "-x", "--audio-format", "mp3",
                 "--ffmpeg-location", ffmpegPath,
-                "--format", "bestaudio/best",
+                "--format", "ba/b",
                 "--user-agent",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
                 "--add-header", "Accept-Language: en-US,en;q=0.9",
                 "--add-header", "Referer: https://www.youtube.com/",
+                "--extractor-args", "youtube:player_client=tv,web_embedded",
                 "--geo-bypass",
                 "-o", fileName));
 
