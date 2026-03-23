f36084c6e01f9abc2425fe127a809349a647acc4
baonoob1011 <trandinhbao222@gmail.com>
2026-03-23 19:43:44 +0700

    Update YoutubeVideoService.java

--- learningApp/src/main/java/com/example/learningApp/service/video/YoutubeVideoService.java
+++ learningApp/src/main/java/com/example/learningApp/service/video/YoutubeVideoService.java
@@ -260,9 +260,8 @@ public class YoutubeVideoService {
                 "--format", "ba/b",
                 "--user-agent",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-                "--add-header", "Accept-Language: en-US,en;q=0.9",
                 "--add-header", "Referer: https://www.youtube.com/",
-                "--extractor-args", "youtube:player_client=tv,web_embedded",
+                "--extractor-args", "youtube:player_client=ios,tv,web_embedded",
                 "--geo-bypass",
                 "-o", fileName));
 
